f5e68647f91fdddf2526be35fa63b0fe61dfdb45
Adrian Chui <adrian.k.chui@gmail.com>
2024-08-02 19:23:18 -0400

    Updating the README

--- camera.py
+++ camera.py
@@ -9,9 +9,6 @@ import ollama
 import asyncio
 from ollama import AsyncClient
 import time
-#from gtts import gTTS
-#import pyttsx3
-#engine = pyttsx3.init()
 
 def gstreamer_pipeline(
     capture_width=1280,
@@ -44,7 +41,6 @@ def gstreamer_pipeline(
 
 video = cv2.VideoCapture(gstreamer_pipeline(flip_method=2), cv2.CAP_GSTREAMER)
 
-a = 0
 img_counter = 0
 in_progress = False
 message = ""
@@ -56,43 +52,27 @@ def chat(frame):
     in_progress = True
     is_success, buffer = cv2.imencode(".jpg", frame)
     io_buf = io.BytesIO(buffer)
-    print("sending image to Ollama")
-    print(time.gmtime())
-    response = ollama.generate('llava:7b', 'If you see me, describe me in one sentence. Otherwise, describe the background in one sentence.',
+    response = ollama.generate('llava:7b', 'If you see me, give me a humorous description of me in one sentence. Otherwise, describe the background in one sentence.',
                                images=[io_buf], stream=False, options=dict(num_predict=30))
-    print(response['response'], end='', flush=True)
     message = response['response']
-    #engine.say(message)
-    #engine.runAndWait()
-    #output = gTTS(text=message, lang='en', slow=False)
-    #output.save("message.mp3")
-    #os.system('mpg123  message.mp3')
 
     subprocess.run('echo "' + message + '"| piper --model en_US-lessac-medium.onnx --output_file message.wav', shell=True)
     subprocess.Popen('aplay message.wav', shell=True)
-
 
     in_progress = False
 
 while True:
 
-    a = a + 1
     check, frame = video.read()
-    #cv2.imwrite("test.png", frame)
     if not check:
         continue
-    # frame = cv2.resize(frame, (960, 540))
-    copy = frame.copy()
-    cv2.putText(frame, message, (0,20),  cv2.FONT_HERSHEY_SIMPLEX, 0.5,(0, 0, 0), 2)
     cv2.imshow("Ollava", frame)
-    #base64_image = base64.b64encode(frame)
 
 
     if not in_progress:
-        threading.Thread(target=chat, args=(copy, )).start()
+        threading.Thread(target=chat, args=(frame, )).start()
 
 
-    # cv2.waitK0ey(0)
     key = cv2.waitKey(1)
 
     if key == ord('q'):
